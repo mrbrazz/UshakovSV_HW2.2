7ffa6ff711dd742a09973388ba59ca58e4063a4c
mrbrazz <mrbrazz@yandex.ru>
2021-07-09 23:32:14 +0300

    test slider alpha properties

--- UshakovSV_HW2.2/ViewController.swift
+++ UshakovSV_HW2.2/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IB Outlet
+
+    @IBOutlet weak var viewWindow: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.value = 1
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
     }
 
-
+    // IB Action
+    @IBAction func redSliderAction() {
+        let redSliderValue = CGFloat(redSlider.value)
+        view.backgroundColor = view.backgroundColor?.withAlphaComponent(redSliderValue)
+    }
+    @IBAction func greenSliderAction() {
+    }
+    @IBAction func blueSliderAction() {
+    }
+    
+    
+    
 }
 
