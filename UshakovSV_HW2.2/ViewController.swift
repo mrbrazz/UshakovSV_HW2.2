5e5cc5a2ec03b0de40f3fe11ae7f347da56261db
mrbrazz <mrbrazz@yandex.ru>
2021-07-11 14:47:24 +0300

    Finish HW2.2

--- UshakovSV_HW2.2/ViewController.swift
+++ UshakovSV_HW2.2/ViewController.swift
@@ -9,56 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //MARK - IB Outlet
+    //MARK: - IB Outlet
     @IBOutlet weak var colorView: UIView!
     
+
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
-    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var redLable: UILabel!
     @IBOutlet weak var greenLable: UILabel!
-    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var blueLable: UILabel!
     
     @IBOutlet weak var redSliderValue: UILabel!
     @IBOutlet weak var greenSliderValue: UILabel!
     @IBOutlet weak var blueSliderValue: UILabel!
     
     
+    //MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //MARK - Red Properties
-        redLabel.text = "Red"
-        redLabel.textColor = .red
-        redSlider.tintColor = .red
-        redSliderValue.text = String(redSlider.value)
-        
-        //MARK - Green Properties
-        greenLable.text = "Green"
-        greenLable.textColor = .green
-        greenSlider.tintColor = .green
-        greenSliderValue.text = String(greenSlider.value)
-        
-        //MARK - Blue Properties
-        blueLabel.text = "Blue"
-        blueLabel.textColor = .blue
-        blueSlider.tintColor = .blue
-        blueSliderValue.text = String(blueSlider.value)
-        
+        setupUI()
     }
     
+
+    //MARK: - Functions
     func changeSliderValue() {
-        redSliderValue.text = String(redSlider.value)
-        greenSliderValue.text = String(greenSlider.value)
-        blueSliderValue.text = String(blueSlider.value)
+        redSliderValue.text = String(format: "%.2f", redSlider.value)
+        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
+        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
     }
     
    func changeColor() {
         colorView.backgroundColor = UIColor(red: CGFloat((redSlider.value)), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
     
-    //MARK - IB Action
+    //MARK: - Private Methods
+    private func setupUI() {
+        colorView.layer.cornerRadius = 20
+        
+        //MARK: - Red Properties
+        redLable.text = "Red"
+        redLable.textColor = .red
+        redSlider.tintColor = .red
+        redSliderValue.text = String(redSlider.value)
+        
+        //MARK: - Green Properties
+        greenLable.text = "Green"
+        greenLable.textColor = .green
+        greenSlider.tintColor = .green
+        greenSliderValue.text = String(greenSlider.value)
+        
+        //MARK: - Blue Properties
+        blueLable.text = "Blue"
+        blueLable.textColor = .blue
+        blueSlider.tintColor = .blue
+        blueSliderValue.text = String(blueSlider.value)
+    }
+    
+    //MARK: - IB Action
     @IBAction func rgbSliderChange() {
         changeColor()
         changeSliderValue()
